215d958adc89d750b1f25392f8cdd6c8469c145d
Zuber Shekh <zuber.shekh7@gmail.com>
2021-12-02 18:50:39 +0530

    add github search api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+
+let url = "https://api.github.com";
+const GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN;
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleChange = (e) => {
+    if (e.target.value !== "") {
+      setUsername(e.target.value);
+    }
+  };
+
+  const fetchData = async (username) => {
+    const response = await fetch(`${url}/users/${username}`, {
+      headers: {
+        Authorization: `Token ${GITHUB_API_TOKEN}`,
+      },
+    });
+    const json = await response.json();
+    setData(json);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData(username);
+  };
+
+  const renderData = () => {
+    if (data) {
+      return <h2>{data.name}</h2>;
+    } else {
+      return <h2>No Data Found</h2>;
+    }
+  };
   return (
     <div>
       <h1>GitHub Search App</h1>
+      <input
+        value={username}
+        type="search"
+        onChange={handleChange}
+        placeholder="Enter Username"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      {data && renderData()}
     </div>
   );
 };
